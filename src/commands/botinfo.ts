fc667989bfb55287cf588152870c53e4525da8c4
Cashew <tungbinem@gmail.com>
2023-05-02 06:42:03 +0900

    feat: track bot uptime and integrate ping into botinfo

--- src/commands/botinfo.ts
+++ src/commands/botinfo.ts
@@ -2,22 +2,29 @@ import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { EMBED_NEUTRAL_COLOR } from '../common/constants';
 import type Command from '../types/command';
 
-const description = "Shows the bot's ping";
+const description = "Shows the bot's status";
 
 const command: Command = {
-  id: 'ping',
-  command: new SlashCommandBuilder().setName('ping').setDescription(description),
-  cmdNames: ['ping'],
+  id: 'botinfo',
+  command: new SlashCommandBuilder().setName('status').setDescription(description),
+  cmdNames: ['botinfo'],
   description,
   help: new EmbedBuilder()
     .setColor(EMBED_NEUTRAL_COLOR)
-    .setTitle('Ping')
+    .setTitle('Bot Status')
     .setDescription(description),
 
   execute: async (interaction) => {
     const reply = await interaction.reply({
       embeds: [new EmbedBuilder().setColor(EMBED_NEUTRAL_COLOR).setTitle('Pong!')],
     });
+
+    const { uptime } = interaction.client;
+    const days = Math.floor(uptime / 86400000);
+    const hours = Math.floor(uptime / 3600000) % 24;
+    const minutes = Math.floor(uptime / 60000) % 60;
+    const seconds = Math.floor(uptime / 1000) % 60;
+
     const embed = new EmbedBuilder()
       .setColor(EMBED_NEUTRAL_COLOR)
       .setTitle('Pong!')
@@ -30,6 +37,11 @@ const command: Command = {
         {
           name: 'API',
           value: `${interaction.client.ws.ping}ms`,
+          inline: true,
+        },
+        {
+          name: 'Uptime',
+          value: `${days}d ${hours}h ${minutes}m ${seconds}s`,
           inline: true,
         },
       );
